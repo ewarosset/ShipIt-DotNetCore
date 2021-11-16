22e0659e0d60175c0627b38a21a6197e10ceb862
Ewa <ewa.rosset@softwire.com>
2021-11-16 11:26:16 +0000

    Added a new data model for the new query

---
+++ ShipIt/Models/DataModels/WarehouseStockDataModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ShipIt.Models.DataModels
+{
+    public class WarehouseStockDataModel : DataModel
+    {
+        [DatabaseColumnName("p_id")]
+        public int ProductId { get; set; }
+        [DatabaseColumnName("w_id")]
+        public int WarehouseId { get; set; }
+        [DatabaseColumnName("hld")]
+        public int held { get; set; }
+
+        public StockDataModel(IDataReader dataReader): base(dataReader) { }
+        
+        public StockDataModel() {}
+    }
+}
