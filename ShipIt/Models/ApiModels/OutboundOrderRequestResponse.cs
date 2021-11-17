9e83f47d487752b2fbb3e79948f657f6d53f074f
Ewa <ewa.rosset@softwire.com>
2021-11-17 10:55:35 +0000

    Trucks function separated

---
+++ ShipIt/Models/ApiModels/OutboundOrderRequestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ShipIt.Models.ApiModels
+{
+    public class OrdersByTruck
+        {
+            public int TruckNumber { get; set; }
+
+            public List<OrderLine> Orders { get; set; }
+            public float TruckLoadInKg { get; set; }
+        }
+
+        public class OutboundOrdersRequest
+        {
+            public int TrucksNeeded { get; set; }
+            public List<OrdersByTruck> OrdersByTruck { get; set; }
+        }
+    }
---
+++ ShipIt/Services/OutboundServivces.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ShipIt.Exceptions;
+using ShipIt.Models.ApiModels;
+using ShipIt.Repositories;
+
+namespace ShipIt_DotNetCore.Services
+{
+    public class OutboundServivces
+    {
+        public static List<String> AddUniqueGtin(OutboundOrderRequestModel request)
+        {
+            var gtins = new List<String>();
+            foreach (var orderLine in request.OrderLines)
+            {
+                if (gtins.Contains(orderLine.gtin))
+                {
+                    throw new ValidationException(String.Format("Outbound order request contains duplicate product gtin: {0}", orderLine.gtin));
+                }
+                gtins.Add(orderLine.gtin);
+            }
+            
+            return gtins;
+        }
+        
+        public static int CalculateTrucksNeeded(List<OrderLine> orderLines, Dictionary<string, Product> products)
+        {
+            var orderWeight = orderLines.Select(line => line.quantity * products[line.gtin].Weight).Sum();
+            var truckCapacity = 2000000;
+            
+            var trucksNeeded = (int) Math.Ceiling(orderWeight / truckCapacity);
+            return trucksNeeded;
+        }
+    }
+}
